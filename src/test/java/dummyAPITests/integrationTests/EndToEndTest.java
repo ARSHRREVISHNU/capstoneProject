3e7a47c7261ab17959018a1fff178fd11870beb0
TestVagrant <testvagrant@TestVagrants-MacBook-Pro.local>
2022-09-17 10:53:23 +0530

    End to end test

---
+++ src/test/java/dummyAPITests/integrationTests/EndToEndTest.java
@@ -0,0 +1,36 @@
+package dummyAPITests.integrationTests;
+
+import dummyAPITests.Request.CreatePostRequestBody;
+import dummyAPITests.Request.CreateUserRequestBody;
+import dummyAPITests.Response.CreatePostResponseBody;
+import dummyAPITests.Response.CreateUserResponseBody;
+import dummyAPITests.Response.DeletePostResponseBody;
+import dummyAPITests.Response.GETDeletedPostResponseBody;
+import dummyAPITests.Users.UsersServices;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class EndToEndTest {
+    UsersServices usersServices;
+@BeforeClass
+public void before(){
+
+    usersServices = new UsersServices();
+}
+
+  @Test
+    public void integrationTests(){
+
+    //Create the post
+    CreatePostRequestBody createPostRequestBody = new CreatePostRequestBody.Builder().build();
+    CreatePostResponseBody createPostResponseBody = usersServices.createPostUnderId(createPostRequestBody);
+    createPostResponseBody.assertCreatedPost(createPostRequestBody);
+    //Deletes the post
+    DeletePostResponseBody deletePostResponseBody = usersServices.deletePostById(createPostResponseBody.getId());
+    deletePostResponseBody.assertDeletedPost(createPostResponseBody.getId());
+    //Checking the deleted post
+      GETDeletedPostResponseBody getDeletedPostResponseBody = usersServices.getDeletedPost(createPostResponseBody.getId());
+      getDeletedPostResponseBody.assertDeletedPost("RESOURCE_NOT_FOUND");
+  }
+
+}
